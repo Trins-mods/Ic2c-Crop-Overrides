9cdbff9d1f0c3b0eaf73054358abb3bd5f66b04b
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-08-05 01:32:38 -0400

    added canola crop

--- src/main/java/trinsdar/resource_crops/Icons.java
+++ src/main/java/trinsdar/resource_crops/Icons.java
@@ -11,5 +11,26 @@ public class Icons {
     public static void loadSprites(){
         Ic2Icons.addSprite(new Sprites.SpriteData("crops", ResourceCrops.MODID + ":textures/crops.png", new Sprites.SpriteInfo(7, 1)));
         Ic2Icons.addTextureEntry(new Sprites.TextureEntry("crops", 0, 0, 7, 1));
+        createAASprite("block_canola_stage_1");
+        createAASprite("block_canola_stage_2");
+        createAASprite("block_canola_stage_3");
+        createAASprite("block_canola_stage_4");
+        createAASprite("block_rice_stage_1");
+        createAASprite("block_rice_stage_2");
+        createAASprite("block_rice_stage_3");
+        createAASprite("block_rice_stage_4");
+        createAASprite("block_rice_stage_5");
+        createAASprite("block_rice_stage_6");
+        createAASprite("block_flax_stage_1");
+        createAASprite("block_flax_stage_2");
+        createAASprite("block_flax_stage_3");
+        createAASprite("block_flax_stage_4");
+        createAASprite("block_flax_stage_5");
+        createAASprite("block_flax_stage_6");
+    }
+
+    public static void createAASprite(String id){
+        Ic2Icons.addSprite(new Sprites.SpriteData(id, "actuallyadditions:blocks/" + id + ".png", new Sprites.SpriteInfo(1, 1)));
+        Ic2Icons.addTextureEntry(new Sprites.TextureEntry(id, 0, 0, 1, 1));
     }
 }
---
+++ src/main/java/trinsdar/resource_crops/crops/CropCanola.java
@@ -0,0 +1,82 @@
+package trinsdar.resource_crops.crops;
+
+import de.ellpeck.actuallyadditions.mod.items.InitItems;
+import ic2.api.crops.CropProperties;
+import ic2.api.crops.ICropTile;
+import ic2.core.block.crop.crops.CropCardBase;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class CropCanola extends CropCardBase {
+    public CropCanola() {
+        super(new CropProperties(2, 1, 1, 0, 5, 1));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public TextureAtlasSprite getTexture(int state) {
+        return this.getSprite("block_canola_stage_" + state)[0];
+    }
+
+    @Override
+    public String getOwner() {
+        return "actuallyadditions";
+    }
+
+    @Override
+    public String getId() {
+        return "canola";
+    }
+
+    @Override
+    public String getDiscoveredBy() {
+        return "Oly206";
+    }
+
+    @Override
+    public int getMaxSize() {
+        return 4;
+    }
+
+    @Override
+    public String[] getAttributes() {
+        return new String[]{"Canola", "Flower", "Yellow"};
+    }
+
+    @Override
+    public boolean canGrow(ICropTile cropTile) {
+        return cropTile.getCurrentSize() <= 3 && cropTile.getLightLevel() >= 10;
+    }
+
+    @Override
+    public boolean canBeHarvested(ICropTile cropTile) {
+        return cropTile.getCurrentSize() == 4;
+    }
+
+    @Override
+    public ItemStack getGain(ICropTile crop) {
+        return new ItemStack(InitItems.itemMisc, 1, 13);
+    }
+
+    @Override
+    public ItemStack getSeeds(ICropTile crop) {
+        return new ItemStack(InitItems.itemCanolaSeed);
+    }
+
+    @Override
+    public int getSizeAfterHarvest(ICropTile cropTile) {
+        return 3;
+    }
+
+    @Override
+    public int getGrowthDuration(ICropTile cropTile) {
+        return cropTile.getCurrentSize() == 3 ? 600 : 400;
+    }
+
+    @Override
+    public int getOptimalHarvestSize(ICropTile cropTile) {
+        return 4;
+    }
+}
