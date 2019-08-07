4f64cce9fa61123b72b00a11fb913cd27845b784
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-08-07 01:13:49 -0400

    registered the new crops

--- src/main/java/trinsdar/resource_crops/Crops.java
+++ src/main/java/trinsdar/resource_crops/Crops.java
@@ -1,15 +1,20 @@
 package trinsdar.resource_crops;
 
+import com.sun.org.apache.xml.internal.security.Init;
+import de.ellpeck.actuallyadditions.mod.items.InitItems;
 import ic2.api.classic.crops.ClassicCrops;
 import ic2.api.crops.CropCard;
 import mekanism.common.MekanismItems;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import trinsdar.resource_crops.crops.CropCanola;
 import trinsdar.resource_crops.crops.CropEmpoweredBlockOnly;
 import trinsdar.resource_crops.crops.CropBlockOnly;
+import trinsdar.resource_crops.crops.CropFlax;
 import trinsdar.resource_crops.crops.CropMiscBlockOnly;
 import trinsdar.resource_crops.crops.CropHardenedGlass;
+import trinsdar.resource_crops.crops.CropRice;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -23,6 +28,9 @@ public class Crops {
     public static CropCard cropOsmia = registerCrop(new CropBlockOnly("Osmia", 8, 2, new String[]{"LightBlue", "Leaves", "Metal"}, new ItemStack(MekanismItems.Dust, 1, 2), 1000, 2200, "Osmium", 4), new ItemStack(MekanismItems.Dust, 1, 2));
     public static CropCard cropSheldia = registerCrop(new CropHardenedGlass("Sheldia", 9, 2, new String[]{"Cerulean", "Leaves", "Metal"}, getModMetaItem("thermalfoundation", "material", 70, 1), 1200, 2400, 6, 5), getModMetaItem("thermalfoundation", "material", 70, 1));
     public static CropCard cropQuartzanthemum = registerCrop(new CropMiscBlockOnly("Quartzanthemum", 5, 1, new String[]{"White", "Leaves", "Metal", "Gem"}, 2, new ItemStack(Items.QUARTZ), 600, 1800, 6), new ItemStack(Items.QUARTZ));
+    public static CropCard cropCanola = registerCrop(new CropCanola(), new ItemStack(InitItems.itemMisc, 1, 13));
+    public static CropCard cropFlax = registerCrop(new CropFlax(), new ItemStack(Items.STRING));
+    public static CropCard cropRice = registerCrop(new CropRice(), new ItemStack(InitItems.itemFoods, 1, 16));
 
     public static void registerCrops(){
         ClassicCrops crop = ClassicCrops.instance;
@@ -30,6 +38,9 @@ public class Crops {
             crop.registerCrop(card);
             crop.registerCropDisplayItem(card, toRegister.get(card));
         }
+        crop.registerBaseSeed(new ItemStack(InitItems.itemCanolaSeed), cropCanola,  1, 1, 1, 1);
+        crop.registerBaseSeed(new ItemStack(InitItems.itemFlaxSeed), cropFlax, 1, 1, 1, 1);
+        crop.registerBaseSeed(new ItemStack(InitItems.itemRiceSeed), cropRice, 1, 1, 1, 1);
     }
 
     public static CropCard registerCrop(CropCard card, ItemStack displayItem){
